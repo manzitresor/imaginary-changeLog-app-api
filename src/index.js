f6f95ecd0c718be64de40411fd7f6da7aaa33caf
manzitresor <manzitre@gmail.com>
2024-08-27 08:13:57 +0200

    chore: install Prisma ORM and configure typescript

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-const http = require('http');
+const app = require('./server');
 
-const server = http.createServer((req,res)=>{
-    if(req.method === "GET" && req.url === "/"){
-        res.writeHead(200,{"Content-Type": "application/json"})
-        res.write(JSON.stringify({message: 'Hello there'}))
-        res.end();
-    }
-})
-
-server.listen(3001,()=>{
-    console.log('server started on port 3001');
+app.listen(3001,() => {
+    console.log('Server running  on Port 3001...')
 })
--- src/server.js
+++ src/server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const app = express();
+const db = 'postgresql://api_course_v4_keuc_user:wn4Sa4dRrb5LJjfOU8nBq2Ialuwo1lLd@dpg-cr6mpnt6l47c7396b5ng-a.oregon-postgres.render.com/api_course_v4_keuc'
 
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.json(JSON.stringify({text: "Hello Express"}))
+})
+
+module.exports = app;
